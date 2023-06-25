f0c59fb66771c37011a6cf5c9529f3bfe049c360
hyukjunkim1116 <nmkk1234@naver.com>
2023-06-25 23:49:22 +0900

    content-type

--- static/js/articleCreate.js
+++ static/js/articleCreate.js
@@ -120,7 +120,7 @@ async function postArticle() {
 	if (file) {
 		const responseURL = await fetch(`${BACKEND_BASE_URL}/articles/get-url/`, {
 			headers: {
-				Accept: "application / json"
+				"content-type": "application/json"
 			},
 			method: "POST"
 		});
