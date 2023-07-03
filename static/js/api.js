b0cf9be10c980e53ebc50f83f0f422895cfc72c4
hyukjunkim1116 <nmkk1234@naver.com>
2023-07-03 20:28:55 +0900

    api.js

--- static/js/api.js
+++ static/js/api.js
@@ -1,5 +1,5 @@
-const FRONT_BASE_URL = "http://127.0.0.1:5500";
-const BACKEND_BASE_URL = "http://127.0.0.1:8000";
+const FRONT_BASE_URL = "http://cookai.today";
+const BACKEND_BASE_URL = "https://www.backend.cookai.today";
 
 // 로그인
 async function handleLogin() {
