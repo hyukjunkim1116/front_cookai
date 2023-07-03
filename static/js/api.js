1744ffc7b2d312f3ae23d959a76ed6ceca9b88b6
hyukjunkim1116 <nmkk1234@naver.com>
2023-07-03 21:14:06 +0900

    🐛Fix: 패스워드 버그수정 #3

--- static/js/api.js
+++ static/js/api.js
@@ -64,6 +64,11 @@ async function handleEmailConfirm() {
 			email: email
 		})
 	});
+	if (response.status == 200) {
+		alert("이메일인증을 진행해주세요!");
+	} else {
+		alert("일치하는 이메일이 없습니다!");
+	}
 	return response;
 }
 
@@ -86,10 +91,10 @@ async function handleChangePasswordConfirm() {
 		})
 	});
 	if (response.status == 200) {
-		alert("비밀번호 변경 완료!");
-		window.location.replace(`${FRONT_BASE_URL}/`);
+		alert(response.message);
+		window.location.replace(`${FRONT_BASE_URL}/login.html`);
 	} else {
-		alert("다시 입력하세요!");
+		alert(response.error);
 	}
 }
 
